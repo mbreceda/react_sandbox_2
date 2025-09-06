36f43c4e0f4c935d3b67f2989ca8223121a5bd2f
Miguel Breceda de Lara <miguel.breceda@protonmail.com>
2025-09-06 08:55:32 -0600

    Update Main Theme configs theme

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,13 +1,27 @@
 import type { Preview } from "@storybook/react";
+import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
 import "../src/index.css";
+import { fn } from "@storybook/test";
 
 const preview: Preview = {
   parameters: {
+    actions: { action: fn },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+    },
+    backgrounds: {
+      default: "light",
+      values: [
+        { name: "light", value: "#ffffff" },
+        { name: "dark", value: "#333333" },
+      ],
+    },
+    viewport: {
+      defaultViewport: "responsive",
+      viewports: INITIAL_VIEWPORTS,
     },
   },
 };
