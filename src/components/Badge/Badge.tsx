e9b111ce027cf5b4514bfdef76d4a0b2a9bdab08
Miguel Breceda de Lara <miguel.breceda@protonmail.com>
2025-09-08 20:04:13 -0600

    Relocate Button, Badge and Review to compoments dir

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -1,13 +1,13 @@
 import styled, { css } from "styled-components";
 
 const Container = styled.div(
-  () => css`
+  ({ theme: { color } }) => css`
     padding: 0.5em 1em;
-    background: #e9e9e9;
+    background: ${color.badgeBackground}
     border-radius: 1em;
     display: inline-block;
     span {
-      color: #636363;
+      color: ${color.badgeText};
     }
     span:first-letter {
       text-transform: capitalize;
